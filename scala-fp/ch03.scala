ceabf8e1d5eb7ad926fb485d7bcf0d52ddcf97b6
Chris <chris.cornelison@gmail.com>
2013-12-30 22:04:36 -0600

    Continued work

--- scala-fp/ch03.scala
+++ scala-fp/ch03.scala
@@ -114,8 +114,41 @@ object List {
     case (_, Nil) => Nil
     case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1,h2), zipWith(t1, t2)(f))
   }
+}
 
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+
+object Tree {
+  val t = Branch(Leaf(1), Branch(Branch(Leaf(2),Leaf(4)), Leaf(3)))
+
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l,r) => size(l) + size(r)
+  }
+
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l,r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l,r) => 1 + depth(l) max depth(r) 
+  }
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l,r) => Branch(map(l)(f), map(r)(f)) 
+  }
+
+  // def fold[A,B](t: Tree[A], z: B)(f: (A,B) => B): B = t match {
+// 
+  // } 
 
 }
+
 
 
