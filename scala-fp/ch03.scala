98eff97a27e1e5c2fd36d3d63d6000e1ed32e3f2
Chris <chris.cornelison@gmail.com>
2013-12-31 18:47:45 -0600

    continued work

--- scala-fp/ch03.scala
+++ scala-fp/ch03.scala
@@ -144,9 +144,22 @@ object Tree {
     case Branch(l,r) => Branch(map(l)(f), map(r)(f)) 
   }
 
-  // def fold[A,B](t: Tree[A], z: B)(f: (A,B) => B): B = t match {
-// 
-  // } 
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B,B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l,r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  } 
+
+  def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t)(a => 1)(1 + _ + _) 
+
+  def maximumViaFold(t: Tree[Int]): Int = 
+    fold(t)(a => a)((l,r) => l max r)
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(a => 0)((l,r) => 1 + (l max r))
+
+  def mapViaFold[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 
 }
 
