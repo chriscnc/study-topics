5656a40421343240f7ebc3a3c8dacd0dd10ce335
Chris <chris.cornelison@gmail.com>
2013-12-31 21:31:24 -0600

    continued work

--- haskell/ch07.hs
+++ haskell/ch07.hs
@@ -1,4 +1,5 @@
 import Data.List
+import qualified Data.Map as Map
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -6,12 +7,7 @@ numUniques = length . nub
 data Person = Person { firstName :: String
                      , lastName :: String
                      , age :: Int
-                     , height :: Float
-                     , phoneNumber :: String
-                     , flavor :: String 
-                     } deriving(Show)
-
-guy = Person "Buddy" "Finkelstein" 43 184.3 "276-0442" "Chocolate"
+                     } deriving(Eq, Show, Read)
 
 
 data Option a = None | Some a deriving(Show)
@@ -26,3 +22,33 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 
 scalarMult :: (Num t) => Vector t -> Vector t -> t
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday   
+           deriving (Eq, Ord, Show, Read, Bounded, Enum) 
+
+
+data LockerState = Taken | Free deriving(Show,Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code  
+lockerLookup lockerNumber map =   
+    case Map.lookup lockerNumber map of   
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"  
+        Just (state, code) -> if state /= Taken   
+                                then Right code  
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!" 
+
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ]  
+
+    
