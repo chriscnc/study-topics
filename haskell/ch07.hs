84dbbb7de9008fb39015bf32ecfd98f4813e2467
Chris <chris.cornelison@gmail.com>
2013-12-31 19:51:34 -0600

    continuing work

--- haskell/ch07.hs
+++ haskell/ch07.hs
@@ -2,3 +2,27 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String 
+                     } deriving(Show)
+
+guy = Person "Buddy" "Finkelstein" 43 184.3 "276-0442" "Chocolate"
+
+
+data Option a = None | Some a deriving(Show)
+
+data Vector a = Vector a a a deriving(Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
